7cacddd0ece946a6ff3dc8f30698e27d2ed17893
Raul Gamero <raul.a.gamero@gmail.com>
2025-03-08 13:15:58 +0100

    .

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Necesario para que Next.js exporte el sitio como estático
 };
 
 export default nextConfig;
