4a4b11fad5dd4dada6ee73f0b3bca3eaf14c403d
Raul Gamero <raul.a.gamero@gmail.com>
2025-03-08 13:43:56 +0100

    done

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,11 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+const compat = new FlatCompat();
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    ignores: ["out/", ".next/", "node_modules/"], // ⬅️ Ignorar estas carpetas
+    ignores: ["out/", ".next/", "node_modules/"], // ⬅️ ESLint ahora ignorará estas carpetas
   },
 ];
 
