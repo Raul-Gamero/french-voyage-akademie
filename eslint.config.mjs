0ee2f47fefee502cb1d83d4da4a5cfb2bd1d8c8e
Raul Gamero <raul.a.gamero@gmail.com>
2025-03-08 13:29:36 +0100

    .

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,9 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: ["out/", ".next/", "node_modules/"], // ⬅️ Ignorar estas carpetas
+  },
 ];
 
 export default eslintConfig;
